8787b062261e3b27ff7eb40addde9a23d539b9b3
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-09-25 00:40:19 +0300

    adding First Steps in Coding

---
+++ 01.First Steps in Coding/04.Online Sumator/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04.Online_Sumator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Calculate(decimal num1, decimal num2)
+        {
+            this.ViewBag.num1 = num1;
+            this.ViewBag.num2 = num2;
+            var sum = num1 + num2;
+            this.ViewBag.sum = sum;
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 01.First Steps in Coding/04.Online Sumator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_04.Online_Sumator.Startup))]
+namespace _04.Online_Sumator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
