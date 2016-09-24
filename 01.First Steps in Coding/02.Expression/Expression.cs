cb8c3de1f4b13fa69574e1c60a627a43d1c4a083
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-09-24 23:43:23 +0300

    Expression Deleted

--- 01.First Steps in Coding/02.Expression/Expression.cs
+++ 01.First Steps in Coding/02.Expression/Expression.cs
@@ -10,7 +10,8 @@ namespace Expression
     {
         static void Main(string[] args)
         {
-           Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+            var expression = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
+           Console.WriteLine(expression);
         }
     }
 }
